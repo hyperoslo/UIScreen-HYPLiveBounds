89342dced6f0054da8f6b30f3195b771bd42fcb8
Christoffer Winterkvist <chris@zenangst.com>
2014-07-05 18:36:20 +0200

    Add Xcode project for testing

---
+++ UIScreen-HYPLiveBoundsTests/UIScreen_HYPLiveBoundsTests.m
@@ -0,0 +1,34 @@
+//
+//  UIScreen_HYPLiveBoundsTests.m
+//  UIScreen-HYPLiveBoundsTests
+//
+//  Created by Christoffer Winterkvist on 7/5/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIScreen_HYPLiveBoundsTests : XCTestCase
+
+@end
+
+@implementation UIScreen_HYPLiveBoundsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
